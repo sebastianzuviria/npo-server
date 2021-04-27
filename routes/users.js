178e3baa9c5e410665ce8ee59e1541aff4117e7a
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-27 17:05:26 -0300

    Added the validation middleware to check the name, lastname, email and password the client sent.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult, check } = require('express-validator');
+
+// Import models
+const User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +11,20 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST a new user (register) */
-router.post('/auth/register', (req, res, next) => {
-  res.send('Hello from register endpoint.');
+router.post('/auth/register', [
+  check('firstName', 'The username is not valid').exists(),
+  check('lastName', 'The last name is not valid').exists(),
+  check('email', 'The email is not valid').exists().isEmail(),
+  check('password', 'The password is not valid').exists().isLength({min: 6})
+], (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  // Check for validation errors
+  if (!validationErrors.isEmpty()) {
+    res.status(400).json({
+      validationErrors: validationErrors.array()
+    })
+  }
 });
 
 module.exports = router;
