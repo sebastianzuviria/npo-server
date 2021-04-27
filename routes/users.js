964a37bd09ed79d9ecbf7893d30269ebf8d56cbe
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-27 15:42:10 -0300

    Added the basic route for authenticating users.

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST a new user (register) */
+router.post('/auth/register', (req, res, next) => {
+  res.send('Hello from register endpoint.');
+});
+
 module.exports = router;
