31b03643f2fcf876b422518a2bb04f2990917fa6
szuviria <szuviria@gmail.com>
2021-04-27 23:08:41 -0300

    Added semicolons to code

--- utils/jsonwebtoken.js
+++ utils/jsonwebtoken.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const loginUser = (dataUser) => {
     const token = jwt.sign(
         dataUser.id, 
-        process.env.SECRET 
-    )
+        process.env.SECRET,
+    );
 
-    return response.status(200).send({token, ...dataUser})
+    return response.status(200).send({token, ...dataUser});
     
-}
+};
 
 const decodeToken = (request) => {  
-    const authorization = request.get('authorization')
+    const authorization = request.get('authorization');
            
     if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        request.token = authorization.substring(7)
-        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        request.token = authorization.substring(7);
+        const decodedToken = jwt.verify(request.token, process.env.SECRET);
         if (request.token || decodedToken.id) {
-            return decodedToken  
+            return decodedToken;  
         } else {
-            return response.status(401).json({ error: 'token invalid' })
+            return response.status(401).json({ error: 'token invalid' });
         }
     } else {
-        return response.status(401).json({ error: 'token missing' })
-    }
-}
+        return response.status(401).json({ error: 'token missing' });
+    };
+};
 
 module.exports = {
     loginUser,
-    decodeToken
-}
+    decodeToken,
+};
