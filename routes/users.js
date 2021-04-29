3b3da168d1fbd3f4929500adbd348961c1cf60d3
gabrielgagz <g.a.gomez@gmail.com>
2021-04-28 23:18:17 -0300

    Add delete user endpoint
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const { deleteUser } = require('../controllers/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* DELETE (soft or logical) from DB */
+router.delete('/:id', deleteUser);
 
 module.exports = router;
