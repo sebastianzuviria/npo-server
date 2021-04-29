90f48bf7d603d160ea9f8ba4b3e2b72a45b47aee
jonathan <jonathangomezit@gmail.com>
2021-04-29 00:22:48 -0300

    update: connection with database

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,18 +1,19 @@
-// const Role = require('../models/role');
-const userList = require('./userList');
+const {Role, User} = require('../models/index');
+
 module.exports = {
 
     getUsers: async (req, res) => {
 
         try{
             
-            // const userList = users.findAll({
-            //     include: {
-            //         model: Role
-            //     },
-            //     attributes:['id', 'firstName', 'lastName', 'email', 'image' ]
+            const userList = await User.findAll({
+                include: [{
+                    model: Role,
+                    as: 'role'
+                }],
+                attributes:['id', 'firstName', 'lastName', 'email', 'image' ]
                 
-            // })
+            })
             
             if(userList.length === 0) throw new Error('The resources do not exist')
             res.status(200).json(userList)
