57d2ceed8e193ccf43af4e280520eca8d0c9a33b
Nataly Mora Mancera <tatamora21@gmail.com>
2021-04-28 23:35:11 -0500

    The infoUser function is added that queries the user's information

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const User = require("../models/user");
+
+const decodeToken = require("../models/user");
+
+const infoUser = async (req, res) => {
+
+
+     try {
+        const user = await User.findAll({
+            attributes: ["firstName", "lastName", "email"],
+            where: {
+                id: req.userid,
+            },
+        });
+
+        if (user.length > 0) {
+            res.status(202).json(user);
+        } else {
+            res.status(404).json({ err: "User not found" });
+        }
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+
+};
+
+module.exports = infoUser;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var infouser =require('../controllers/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/auth/me', function(req, res) {
-  res.send('respond with a resource');
-});
+//Get the information of the authenticated user
+router.get('/auth/me',infouser);
 
 module.exports = router;
