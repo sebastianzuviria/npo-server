a22f9b4e1e590b6bc6f696528c82038e59e44c6c
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-04-30 17:16:01 -0300

    login route updated

--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,11 @@
 const getUser = require('../controllers/getUser');
+const login = require('../controllers/login');
 const isEmail = require('../middlewares/isEmail');
 const passwordLength = require('../middlewares/passwordLength');
 const validateBody = require('../middlewares/validateBody');
-const verifyPassword = require('../utils/verifyPassword');
-//const loginUser = require('../utils/jsonwebtoken);
 
 const router = require('express').Router();
 
-router
-  .route('/login')
-  .post(isEmail, passwordLength, validateBody, async (req, res) => {
-    try {
-      const { email, password } = req.body;
-
-      const user = await getUser('email', email);
-      if (!user) res.status(404).json({ ok: false });
-
-      const passIsOk = await verifyPassword(password, user.password);
-      if (!passIsOk) res.status(400).json({ ok: false });
-
-      // JWT implementation
-      // loginUser(user)
-
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json({ ok: false });
-    }
-  });
+router.route('/login').post(isEmail, passwordLength, validateBody, login);
 
 module.exports = router;
