bc344350f53bdfb960b6f75a5964478609b4128a
Emiliano Rivarola <emi_rivarola@hotmail.com>
2021-05-01 14:08:24 -0300

    i add properties and  values at contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const contacts = [
-    {'id':'1', 'nombre':'Emiliano'},
-    {'id':'2', 'nombre':'Jonathan'},
-    {'id':'3', 'nombre':'Gabriel'},
-    {'id':'4', 'nombre':'Sebastian'},
-    {'id':'5', 'nombre':'Bruno'},
-    {'id':'6', 'nombre':'Nataly'},
-    {'id':'7', 'nombre':'Franco'},
-    {'id':'8', 'nombre':'Ariel'}
+    {'id':'1', 'name':'Emiliano', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'2', 'name':'Jonathan', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'3', 'name':'Gabriel', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'4', 'name':'Sebastian', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'5', 'name':'Bruno', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'6', 'name':'Nataly', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'7', 'name':'Franco', 'email':'email@123.com', 'message':'Hi this is a message'},
+    {'id':'8', 'name':'Ariel', 'email':'email@123.com', 'message':'Hi this is a message'}
 ]
 /* GET users listing. */
 router.get('/contacts', function(req, res, next) {
