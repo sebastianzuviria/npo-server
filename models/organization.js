7b304ba94e701cda1241416cff1141ce6bc8dcde
Nataly Mora Mancera <tatamora21@gmail.com>
2021-05-01 00:53:42 -0500

    Adjusted model name

--- models/organization.js
+++ models/organization.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Organization extends Model {}
-  User.init(
+  Organization.init(
     {
       name: DataTypes.STRING,
       image: DataTypes.STRING,
