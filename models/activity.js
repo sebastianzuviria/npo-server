591e51c5182f98daae8cacb40f2fc59789d7bd69
gabrielgagz <g.a.gomez@gmail.com>
2021-05-01 02:47:05 -0300

    Create activities model and migration
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

---
+++ models/activity.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Activity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Activity.belongsTo(models.User, {as: 'user'});
+    }
+  };
+  Activity.init({
+    content: DataTypes.STRING,
+    image: DataTypes.STRING,
+    name: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    paranoid: true,
+    modelName: 'Activity',
+  });
+  return Activity;
+};
