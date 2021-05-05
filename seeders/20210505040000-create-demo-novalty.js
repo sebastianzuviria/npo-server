fca9a30ee84f25d9564cafc38e2bc89c675e93a1
szuviria <szuviria@gmail.com>
2021-05-05 18:22:35 -0300

    Modified content of novelty seeder

--- seeders/20210505040000-create-demo-novalty.js
+++ seeders/20210505040000-create-demo-novalty.js
@@ -9,12 +9,12 @@ module.exports = {
           title: 'News 1',
           image: 'image.jpg',
           type: 'news',
-          content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+          content: `<p>Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
       velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
       pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
       sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
       Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
-      eros id velit condimentum, eu ultrices nisl consequat.`,
+      eros id velit condimentum, eu ultrices nisl consequat.</p>`,
           categoryId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
@@ -23,12 +23,12 @@ module.exports = {
             title: 'News 2',
             image: 'image2.jpg',
             type: 'news',
-            content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
-        velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
-        pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
-        sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
-        Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
-        eros id velit condimentum, eu ultrices nisl consequat.`,
+            content: `<p>Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+            velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
+            pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
+            sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
+            Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
+            eros id velit condimentum, eu ultrices nisl consequat.</p>`,
             categoryId: 2,
             createdAt: new Date(),
             updatedAt: new Date()
@@ -37,12 +37,12 @@ module.exports = {
             title: 'News 3',
             image: 'image3.jpg',
             type: 'news',
-            content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
-        velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
-        pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
-        sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
-        Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
-        eros id velit condimentum, eu ultrices nisl consequat.`,
+            content: `<p>Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+            velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
+            pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
+            sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
+            Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
+            eros id velit condimentum, eu ultrices nisl consequat.</p>`,
             categoryId: 3,
             createdAt: new Date(),
             updatedAt: new Date()
