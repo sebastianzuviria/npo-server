af264a9cc54d42c9fe2f26660a3321d35e2094f6
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-06 20:05:15 -0300

    Fix: added 'msg' to response if email doesnt exists

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,5 @@
 const { User } = require('../models/index');
 const verifyPassword = require('../utils/verifyPassword');
-//const { loginUser } = require('../utils');
-//const {loginUser} = require('../utils/jsonwebtoken);
 const { signToken } = require('../utils/jsonwebtoken');
 
 const login = async (req, res) => {
@@ -9,19 +7,20 @@ const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
-    if (!user) res.status(404).json({ ok: false });
+    if (!user)
+      return res.status(404).json({ ok: false, msg: 'Email not found' });
 
     const passIsOk = await verifyPassword(password, user.password);
 
     if (!passIsOk) {
-      res.status(400).json({ ok: false, msg: "Password doesn't match" });
+      return res.status(400).json({ ok: false, msg: "Password doesn't match" });
     } else {
       const { password, ...dataForToken } = user.dataValues;
       signToken(dataForToken, res);
     }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ ok: false, err });
+    return res.status(500).json({ ok: false, err });
   }
 };
 module.exports = login;
