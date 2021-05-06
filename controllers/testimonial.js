70c6e6fe2595003b7e8e937388fd16de23a9dd06
Bzch <francobaisch00@gmail.com>
2021-05-06 12:22:21 -0300

    created testimonials controller

---
+++ controllers/testimonial.js
@@ -0,0 +1,71 @@
+const { Testimonial } = require("../models/index");
+const { validationResult } = require("express-validator");
+
+const getTestimonials = async (req, res) => {
+  try {
+    const testimonials = await Testimonial.findAll({
+      where: { type: "testimonials" },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+      include: {
+        model: Testimonial,
+        attributes: ["name", "content"],
+      },
+    });
+    res.status(200).json(testimonials);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getTestimonialById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    testimonial = await Testimonial.findByPk(id, {
+      attributes: ["id", "name", "content"],
+    });
+    if (testimonial) {
+      res.status(200).json(testimonial);
+    } else {
+      res
+        .status(400)
+        .json({ error: "it looks like what you are looking for is not here" });
+    }
+  } catch {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const newTestimonial = async (req, res) => {
+  const body = req.body;
+  const validation = validationResult(req);
+
+  if (!validation.isEmpty()) {
+    res
+      .status(400)
+      .json({
+        validation: validation.array(),
+      })
+      .end();
+  } else {
+    try {
+      const newTestimonial = await Testimonial.create({
+        name: body.name,
+        content: body.content,
+        type: "testimonials",
+      });
+      res.status(201).json(newTestimonial);
+    } catch (error) {
+      res.status(400).json({ status: 400, error: error.message });
+    }
+  }
+};
+
+const testimonialController = {
+  getTestimonials,
+  getTestimonialById,
+  newTestimonial,
+};
+
+module.exports = testimonialController;
