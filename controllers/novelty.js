4156dad4867f31f9658bde49e31e90eb6b039902
szuviria <szuviria@gmail.com>
2021-05-06 07:13:23 -0300

    Created controller to create a new Novelty

--- controllers/novelty.js
+++ controllers/novelty.js
@@ -1,4 +1,5 @@
 const { Novelty } = require('../models/index');
+// const { Category } = require('../models/index')
 
 const getNovelties = async (request, response) => {
 
@@ -13,15 +14,15 @@ const getNovelties = async (request, response) => {
             }, 
             attributes: ['id', 'title', 'image', 'createdAt'],
             order: [['createdAt', 'DESC']]
-        })
-        response.status(200).json(noveltiesReturned)
+        });
+        response.status(200).json(noveltiesReturned);
     } catch (error) {
-        response.status(400).json({ error: error.message })
-    }
-}
+        response.status(400).json({ error: error.message });
+    };
+};
 
 const getNoveltyById = async (request, response) => {
-    const id = request.params.id
+    const id = request.params.id;
 
     try {
         const noveltyReturned = await Novelty.findByPk(id, {
@@ -30,34 +31,57 @@ const getNoveltyById = async (request, response) => {
             //     attributes: ['name']
             // },
             attributes: ['id', 'title', 'image', 'content', 'categoryId', 'type', 'createdAt'],
-        })
+        });
         if (noveltyReturned){ 
-            response.json(noveltyReturned)
+            response.json(noveltyReturned);
         } else {
-            response.status(400).json({ error: 'new not exist' })
+            response.status(400).json({ error: 'new not exist' });
         }
     } catch (error) {
-        response.status(400).json({ error: error.message })
-    }
-}
+        response.status(400).json({ error: error.message });
+    };
+};
 
 const deleteNovelty = async (request, response) => {
-    const id = request.params.id
+    const id = request.params.id;
 
     try {
-        const noveltyToDelete = await Novelty.findByPk(id)
+        const noveltyToDelete = await Novelty.findByPk(id);
         if(noveltyToDelete) {
             await Novelty.destroy({
                 where: {
                     id: id
                 }
-            })
-            response.status(204).json({ message: 'New deleted' })
+            });
+            response.status(204).json({ message: 'New deleted' });
         } else {
-            response.status(400).json({ error: 'New not exist' })
+            response.status(400).json({ error: 'New not exist' });
         }
     } catch (error) {
-        response.status(400).json({ error: error.message })
+        response.status(400).json({ error: error.message });
+    };
+};
+
+const createNovelty = async (request, response) => {
+    const body = request.body;
+
+    try {
+        // const category = await Category.findOne({ where: { name: body.category }});
+        // if(category) {
+            const newNovelty = await Novelty.create({
+                title: body.title,
+                image: body.image,
+                content: body.content,
+                //categoryId: category.id
+                categoryId: 1,
+                type: 'news'
+            });
+            response.status(201).json(newNovelty);
+        // } else {
+        //   response.status(400).json({ error: 'category not exist'});  
+        // }
+    } catch (error) {
+        response.status(400).json({ error: error.message });
     }
 }
 
