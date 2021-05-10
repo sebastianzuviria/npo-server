a697f43d9b77c913622701c2905aada5beaa18aa
jonathan <jonathangomezit@gmail.com>
2021-05-10 19:43:00 -0300

    Fix: conflicts 2

--- routes/members.js
+++ routes/members.js
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 
-const { getMembers } = require('../controllers/members');
+const { getMembers, deleteMember } = require('../controllers/members');
 
 
 router.get('/', getMembers);
 
-module.exports=router;
-=======
-const express = require("express");
-const router = express.Router();
-
-const deleteMember = require('../controllers/members')
-
-
 router.delete("/:id",deleteMember);
 
 module.exports = router;
->>>>>>> 2e0ccebb7dd63c7be72a13605dd65d1fa0a229e1
+
+
+
+
