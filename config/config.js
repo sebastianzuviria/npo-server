78ef0089ee641e1bcb1eecad07e0a44573dca3ba
gabrielgagz <g.a.gomez@gmail.com>
2021-05-13 02:09:51 -0300

    Prepare environment

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
+        "database": process.env.DB_NAME_TEST,
         "host": process.env.DB_HOST,
         "dialect": "mysql"
     },
