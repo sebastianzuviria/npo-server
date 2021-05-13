388a2f001ce5c84675c9fd313d34fadf90eb133b
gabrielgagz <g.a.gomez@gmail.com>
2021-05-13 09:44:15 -0300

    Check if response is JSON. Check all activities.

--- test/activities.test.js
+++ test/activities.test.js
@@ -1,10 +1,67 @@
+const db = require('../models/index');
+const { Activity } = require('../models/index');
+
+const supertest = require('supertest');
+const app = require('../app');
+const apiTest = supertest(app);
+
+
+// Set initial activities
+const initialActivity = [
+    {
+        content: 'Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut velit tempor Proin cursus eleifend pretium.',
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        name: 'Activity n2',
+        userId: 1
+    },
+    {
+        content: 'Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut velit tempor Proin cursus eleifend pretium.',
+        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+        name: 'Activity n3',
+        userId: 1
+    },
+
+];
+
+// Clean DB and add initialActivities before run all tests
+beforeAll( async () => {
+
+    try {
+
+        await db.sequelize.sync({ force: false });
+        await Activity.destroy({ where: {}})
+    
+        initialActivity.map( async (data) => {
+        await Activity.create(data);
+    });
+        
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+    
+});
+
 describe('Activities endpoint tests', () => {
 
-    // This test fails because 1 !== 2
-    it("Test jest", () => {
+    it("Get /activities as JSON", async () => {
 
-    expect(1).toBe(1);
+        await apiTest
+            .get('/activities')
+            .expect('Content-Type', /json/)
+            .expect(200);
+
+    });
+
+    it("Get all /activities", async () => {
+
+        const res = await apiTest.get('/activities');
+        expect(res.body).toHaveLength(initialActivity.length);
 
     });
     
+});
+
+// Close sequelize connection after run all tests
+afterAll(() => {
+    db.sequelize.close()
 });
