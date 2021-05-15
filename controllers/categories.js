b21b95191df6b3c8befe80dc6ebbd97266b9aee2
szuviria <szuviria@gmail.com>
2021-05-15 05:08:48 -0300

    Modified status code and messages

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,66 +1,53 @@
-'use-strict'
-
-// Model
 const { Category } = require('../models/index');
 
 module.exports = {
     getCategories: async (req, res) => {
         try {
-            let categoriesList = await Category.findAll();
+            const categoriesList = await Category.findAll();
 
             if (categoriesList.length === 0) {
                 res.status(404).json({
-                    'message': 'Sorry, there are no categories'
+                    error: 'Sorry, there are not categories'
                 })
             }
             else {
-                res.status(200).json({
-                    'categories': categoriesList
-                })
+                res.status(200).json(categoriesList)
             }
         } 
         catch (error) {
-            res.status(500).json({
-                'message': 'Category not found',
-                'error': error.message
-            });
+            res.status(400).json({ error: error.message });
         }
     },
+    
     createCategory: async (req, res) => {
         const body = req.body;
 
         try {
             // Verificar si la nueva categoria ya existe
-            let existingCategory = await Category.findOne({ where: { name: body.name } });
+            const existingCategory = await Category.findOne({ where: { name: body.name } });
 
             if (!existingCategory) {
-                let newCategory = await Category.create({
+                const newCategory = await Category.create({
                     name: body.name,
                     description: body.description
                 });
 
-                res.status(201).json({
-                    'message': 'New category created successfully',
-                    'newCategory': newCategory
-                });
+                res.status(201).json(newCategory);
             }
             else {
                 res.status(409).json({
-                    'message': 'That category already exists'
+                    message: 'That category already exists'
                 })
             }
         } 
         catch (error) {
-            res.status(500).json({
-                'message': 'Category not created',
-                'error': error
-            });
+            res.status(400).json({ error: error.message });
         }
     },
 
-    updateCategory: async (request, response) => {
-        const id = request.params.id
-        const body = request.body
+    updateCategory: async (req, res) => {
+        const id = req.params.id
+        const body = req.body
         
         try {
             const isUpdatedCategory = await Category.update({
@@ -70,12 +57,12 @@ module.exports = {
 
             if(isUpdatedCategory[0] === 1) {
                 const updatedCategory = await Category.findByPk(id)
-                response.status(200).json(updatedCategory)
+                res.status(200).json(updatedCategory)
             } else {
-                response.status(404).json({ error: 'Category not exist'})
+                res.status(404).json({ error: 'Category not exist'})
             }
         } catch (error) {
-            response.status(400).json({ error: error.message })
+            res.status(400).json({ error: error.message })
         }
     }
 }
