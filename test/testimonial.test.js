5d42a5e83f3385094b5e1d0227c6a3b8e94db108
szuviria <szuviria@gmail.com>
2021-05-15 01:55:55 -0300

    Created testimonial test and added initial testimonials

---
+++ test/testimonial.test.js
@@ -0,0 +1,37 @@
+const db = require('../models/index')
+const { Testimonial } = require('../models/index')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const initialTestimonials = [
+    {
+        name: 'Testimonial 1',
+        content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+        velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
+        pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
+        sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
+        Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
+        eros id velit condimentum, eu ultrices nisl consequat.`
+    },
+    {
+        name: 'Testimonial 2',
+        content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+        velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
+        pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
+        sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
+        Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
+        eros id velit condimentum, eu ultrices nisl consequat.`
+    },
+    {
+        name: 'Testimonial 3',
+        content: `Lorem dolor sit amet, consectetur adipiscing elit. Pellentesque vel mi ut
+        velit tempor aliquam eget eget enim. Proin cursus eleifend pretium. Aliquam cursus 
+        pellentesque interdum. Vivamus placerat id leo a pellentesque. Vivamus a congue urna,
+        sed porta eros. Etiam finibus magna et est aliquam, sed semper libero facilisis. 
+        Donec lectus lorem, rhoncus vitae quam eget, vulputate gravida elit. Praesent ultricies
+        eros id velit condimentum, eu ultrices nisl consequat.`
+    }
+]
+
