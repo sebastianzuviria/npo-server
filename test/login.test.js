bf18e8ab7d045eae20415f3b9a4c6683b61716f7
gabrielgagz <g.a.gomez@gmail.com>
2021-05-15 00:59:47 -0300

    Add remaining tests

--- test/login.test.js
+++ test/login.test.js
@@ -45,9 +45,97 @@ beforeAll( async () => {
 
 describe('Authentication endpoint tests', () => {
 
-    it('Dummy Test', async () => {
+    it('(POST) must properly login', async () => {
 
-        await expect(1).toBe(1);
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'test@test.com' })
+            .send({ password: 'test1234' })
+            .expect(200);
+
+    });
+
+    it('(POST) must fail if email isn\'t found', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'none@none.com' })
+            .send({ password: 'test1234' })
+            .expect(404);
+
+    });
+
+    it('(POST) must fail if email is empty', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: '' })
+            .send({ password: 'test1234' })
+            .expect(400);
+
+        const { msg, param } = JSON.parse(response.text).errors[0];
+
+        expect(param).toBe('email');
+        expect(msg).toBe('Invalid value');
+
+    });
+
+    it('(POST) must fail if email is invalid', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'wrongemail#' })
+            .send({ password: 'test1234' })
+            .expect(400);
+
+        const { msg, param } = JSON.parse(response.text).errors[0];
+
+        expect(param).toBe('email');
+        expect(msg).toBe('Invalid value');
+
+    });
+
+    it('(POST) must fail if password is empty', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'test@test.com' })
+            .send({ password: '' })
+            .expect(400);
+
+        const { msg, param } = JSON.parse(response.text).errors[0];
+
+        expect(param).toBe('password');
+        expect(msg).toBe('Invalid value');
+
+    });
+
+    it('(POST) must fail if password length is invalid', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'test@test.com' })
+            .send({ password: 'tes' })
+            .expect(400);
+
+        const { msg, param } = JSON.parse(response.text).errors[0];
+
+        expect(param).toBe('password');
+        expect(msg).toBe('Invalid value');
+
+    });
+
+    it('(POST) must fail if password doesn\'t match', async () => {
+
+        const response = await apiTest
+            .post(`/auth/login`)
+            .send({ email: 'test@test.com' })
+            .send({ password: 'test1235' })
+            .expect(400);
+
+        const { msg } = JSON.parse(response.text);
+
+        expect(msg).toBe('Password doesn\'t match');
 
     });
 
