eb629507f8d7302743624aa03808bb7893e61c94
Ariel Sanchez <arii.sanchez123@gmail.com>
2021-05-20 20:21:06 -0300

    Added users to seed and DemoUsers.md

---
+++ DemoUsers.md
@@ -0,0 +1,31 @@
+# Datos de prueba de usuarios.
+
+## Usuarios ADMIN
+
+| firstName      | lastName     | email               | password    |
+| -------------- | ------------ | ------------------- | ----------- |
+| Sebastian      | Zuviria      | sebastian@email.com | usuariotest |
+| Emiliano       | Rivarola     | emiliano@email.com  | usuariotest |
+| Alkemy         | Labs         | alkemy@email.com    | usuariotest |
+| Ariel          | Sanchez      | ariel@email.com     | usuariotest |
+| Jonathan Oscar | Molina Gomez | jonathan@email.com  | usuariotest |
+| Hugo           | Foncea       | hugo@email.com      | usuariotest |
+| Nataly Mora    | Mancera      | nataly@email.com    | usuariotest |
+| Franco         | Baisch       | franco@email.com    | usuariotest |
+| Gabriel        | Gomez        | gabriel@email.com   | usuariotest |
+| Team27         | CodeZilla    | team27@email.com    | usuariotest |
+
+## Usuarios Standard
+
+| fistName  | lastName  | email              | password    |
+| --------- | --------- | ------------------ | ----------- |
+| Antonio   | Fernandez | antonio@mail.com   | usuariotest |
+| Jose      | Schilk    | jose@mail.com      | usuariotest |
+| Francisco | Moreno    | francisco@mail.com | usuariotest |
+| Juan      | Navarro   | juan@mail.com      | usuariotest |
+| Manuel    | Garcia    | manuel@mail.com    | usuariotest |
+| Maria     | Serrano   | maria@mail.com     | usuariotest |
+| Josefa    | Ruiz      | josefa@mail.com    | usuariotest |
+| Isabel    | Diaz      | isabel@mail.com    | usuariotest |
+| Noemi     | Romero    | noemi@mail.com     | usuariotest |
+| Lucia     | Saez      | lucia@mail.com     | usuariotest |
--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -1,18 +1,158 @@
 'use strict';
 
+const encryptPassword = require('../utils/encrypt');
+const { User } = require('../models/index');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      firstName: 'Usuario',
-      lastName: 'Demo',
-      email: 'test@test.com',
-      // Important: Password not encrypted yet! 
-      password: '1234',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    }], {});
+    const userObject = {
+      password: await encryptPassword('usuariotest'),
+      firstName: '',
+      lastName: '',
+      email: '',
+      roleId: 2,
+      image:
+        'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const adminUsers = [
+      {
+        ...userObject,
+        firstName: 'Emiliano',
+        lastName: 'Rivarola',
+        roleId: 1,
+        email: 'emiliano@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Franco',
+        lastName: 'Baisch',
+        roleId: 1,
+        email: 'franco@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Gabriel',
+        lastName: 'Gomez',
+        roleId: 1,
+        email: 'gabriel@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Jonathan Oscar',
+        lastName: 'Molina Gomez',
+        roleId: 1,
+        email: 'jonathan@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Nataly Mora',
+        lastName: 'Mancera',
+        roleId: 1,
+        email: 'nataly@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Sebastian',
+        lastName: 'Zuviria',
+        roleId: 1,
+        email: 'sebastian@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Hugo',
+        lastName: 'Foncea',
+        roleId: 1,
+        email: 'hugo@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Ariel',
+        lastName: 'Sanchez',
+        roleId: 1,
+        email: 'ariel@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Alkemy',
+        lastName: 'Labs',
+        roleId: 1,
+        email: 'alkemy@email.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Team27',
+        lastName: 'CodeZilla',
+        roleId: 1,
+        email: 'team27@email.com'
+      }
+    ];
+
+    const regularUsers = [
+      {
+        ...userObject,
+        firstName: 'Antonio',
+        lastName: 'Fernandez',
+        email: 'antonio@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Jose',
+        lastName: 'Schilk',
+        email: 'jose@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Francisco',
+        lastName: 'Moreno',
+        email: 'francisco@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Juan',
+        lastName: 'Navarro',
+        email: 'juan@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Manuel',
+        lastName: 'Garcia',
+        email: 'manuel@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Maria',
+        lastName: 'Serrano',
+        email: 'maria@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Josefa',
+        lastName: 'Ruiz',
+        email: 'josefa@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Isabel',
+        lastName: 'Diaz',
+        email: 'isabel@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Noemi',
+        lastName: 'Romero',
+        email: 'noemi@mail.com'
+      },
+      {
+        ...userObject,
+        firstName: 'Lucia',
+        lastName: 'Saez',
+        email: 'lucia@mail.com'
+      }
+    ];
+    await queryInterface.bulkDelete('Users', {});
+    await queryInterface.bulkInsert('Users', [...adminUsers, ...regularUsers]);
   },
 
   down: async (queryInterface, Sequelize) => {
