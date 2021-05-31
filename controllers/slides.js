25e2359194129be6eed403b2d06d37f538c7200a
gabrielgagz <g.a.gomez@gmail.com>
2021-05-31 03:02:55 -0300

    SLIDES: prepare to use AWS S3 bucket

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,6 +1,7 @@
 'use-strict';
 
 const { Slide } = require('../models/index');
+const imageServices = require('../services/amazonS3/imageServices')
 
     const getSlides = async (req,res)=> {
 
@@ -18,5 +19,43 @@ const { Slide } = require('../models/index');
         }
     };
 
+    const updateSlide = async (req,res)=> {
 
-module.exports = { getSlides }
+        const id = req.params.id;
+        const { text, order } = req.body;
+
+        const urlOfImage = async () => {
+
+            if (req.file) {
+    
+                const url = await imageServices.uploadImage(req.file);
+                await imageServices.deleteImage(imageurl);
+                return url;
+    
+            } else {
+                return imageurl;
+            }
+        }
+
+        try {
+
+            await Slide.update({
+                imageUrl: await urlOfImage(),
+                order,
+                text
+            }, { where: { id } } );
+
+            // Get the updated activity
+            const updatedSlide = await Slide.findByPk( id );
+
+            return ( !updatedSlide ) ? res.status(400).json( { error: 'Slide not Found' } ) : res.status(200).json( updatedSlide );
+            
+        } catch(err) {
+            
+            res.status(400).json({error: err.message})
+
+        }
+    };
+
+
+module.exports = { getSlides, updateSlide }
--- routes/slides.js
+++ routes/slides.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { verifyAdmin } = require('../middlewares/verifyRoles');
-const { getSlides } = require('../controllers/slides')
+const { getSlides, updateSlide } = require('../controllers/slides');
+const imageServices = require('../services/amazonS3/imageServices');
 
 router.get("/:id", getSlides);
+router.put("/:id", verifyAdmin, imageServices.uploadMiddleware, updateSlide);
 
 module.exports = router;
